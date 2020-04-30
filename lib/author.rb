2c44f5ccdf20054197667c99c6b2ddc39ae15941
dustin <djarcene@gmail.com>
2020-04-30 03:00:26 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -3,3 +3,13 @@ class Author
   attr_accessor :name, :posts
   
   @@post_count = 0
+  
+  def initialize(name)
+    @name = name
+    @post = []
+  end
+  
+  def add_post(post)
+    @post << post
+    post.author = self
+    @@post
